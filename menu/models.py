55656a678302e268d4256626e5218702193e2060
Daniel Juarez <juarezdaniel@gmail.com>
2022-02-14 15:58:01 -0500

    final commit

--- menu/models.py
+++ menu/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class MenuItem(models.Model):
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     price = models.IntegerField()
     catagory = models.CharField(max_length=255)
     quantity = models.IntegerField()
     size = models.CharField(max_length=255)
     url = models.ImageField()
 
+    def __str__(self):
+        return self.name
+        
